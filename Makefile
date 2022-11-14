bc13da9076ffb5eae7225162bfdb32d786e66578
Heitor Maciel Pinto <hmaciel-@c1r14s4.42porto.com>
2022-11-14 19:11:07 +0000

    avancos

--- Makefile
+++ Makefile
@@ -6,17 +6,12 @@
 #    By: hmaciel- <hmaciel-@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/11/06 15:56:37 by hmaciel-          #+#    #+#              #
-#    Updated: 2022/11/13 12:00:39 by hmaciel-         ###   ########.fr        #
+#    Updated: 2022/11/14 18:50:49 by hmaciel-         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 SRCS			=	ft_printf.c
 OBJS			= $(SRCS:.c=.o)
-
-BONUS			=	ft_lstadd_back.c ft_lstadd_front.c ft_lstclear.c \
-					ft_lstdelone.c ft_lstiter.c ft_lstlast.c \
-					ft_lstmap.c ft_lstnew.c ft_lstsize.c
-BONUS_OBJS		= $(BONUS:.c=.o)
 
 CC				= cc
 RM				= rm -f
@@ -36,7 +31,4 @@ fclean:			clean
 				$(RM) $(NAME)
 
 re:				fclean $(NAME)
-
-bonus:			$(OBJS) $(BONUS_OBJS)
-				ar rc $(NAME) $(OBJS) $(BONUS_OBJS)
 
--- ft_printf.c
+++ ft_printf.c
@@ -6,15 +6,48 @@
 /*   By: hmaciel- <hmaciel-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 04:32:40 by hmaciel-          #+#    #+#             */
-/*   Updated: 2022/11/13 13:24:23 by hmaciel-         ###   ########.fr       */
+/*   Updated: 2022/11/14 19:08:01 by hmaciel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdarg.h>
+
+int	ft_get_digits(long int nb)
+{
+	int	digits;
+
+	digits = 0;
+	if (nb == 0)
+		return (1);
+	if (nb < 0)
+	{
+		nb *= -1;
+		digits = 1;
+	}
+	while (nb > 0)
+	{
+		digits++;
+		nb /= 10;
+	}
+	return (digits);
+}
 
 static void	ft_putchar_fd(char c, int fd)
 {	
 	write(fd, &c, 1);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	int	len;
+
+	len = 0;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
 }
 
 static void	ft_putnbr_fd(long int np, int fd)
@@ -65,21 +98,24 @@ static char	*ft_strupcase(char *str)
 	return (str);
 }
 
-static void	put_hex(long int np, char x)
+
+static int	put_hex(long int np, char x)
 {
 	char	base[] = "0123456789abcdef";
-	
+	static int digits;
 	if (np < 0)
 		np = 4294967295 - (np * -1) + 1;
 	if (x == 'X')
 		ft_strupcase(base);
-	if (np > 16)
+	if (np >= 16)
 	{
 		put_hex(np / 16, x);
 		put_hex(np % 16, x);
+		digits++;
 	}
 	else
 		write(1, &base[np], 1);
+	return (digits - 1);
 }
 
 static void ft_putunsigned(unsigned int n)
@@ -87,33 +123,47 @@ static void ft_putunsigned(unsigned int n)
 	ft_putnbr_fd(n, 1);
 }
 
-static void ft_check_format(char next, va_list args)
+static int ft_check_format(char next, va_list args)
 {
+	int len;
+
+	len = 0;
 	if (next == '%')
+	{
 		write (1, "%", 1);
+		len = 1;
+	}
 	else if (next == 'c')
 	{
 		char char_to_print;
 		char_to_print = va_arg(args, int);
         write(1, &char_to_print, 1);
+		len = 1;
 	}
 	else if (next == 's')
 	{
 		char	*str;
 		str = va_arg(args, char*);
-		ft_putstr_fd(str, 1);
+		if (!str)
+			len = 0;
+		else
+		{
+			ft_putstr_fd(str, 1);
+			len = (ft_strlen(str));
+		}
 	}
 	else if (next == 'd' || next == 'i')
 	{
 		int	nbr;
 		nbr = va_arg(args, int);
-		ft_putnbr_fd(nbr, 1);		
+		len = ft_get_digits(nbr);
+		ft_putnbr_fd(nbr, 1);
 	}
 	else if (next == 'x' || next == 'X')
 	{
 		int	ret;
 		ret = va_arg(args, int);
-		put_hex(ret, next);
+		return(put_hex(ret, next));
 	}
 	else if (next == 'p')
 	{
@@ -121,71 +171,74 @@ static void ft_check_format(char next, va_list args)
 		ret = va_arg(args, long int);
 		write(1, "0x", 2);
 		put_hex(ret, 'x');
+		len = 14;
 	}
 	else if (next == 'u')
 	{
-		int ret;
-		ret = va_arg(args, int);
+		long int ret;
+		ret = va_arg(args, long int);
+		len = ft_get_digits(ret);
 		ft_putunsigned(ret);
 	}
+	return (len - 2);
 }
-
-static size_t   ft_printf_strlen(const char *s)
-{
-    int len;
-
-    len = 0;
-	if (!s)
-		return (len);
-    while (*s != '\0')
-    {
-        if (*s == '%')
-        {
-          if (*s++ == '\0')
-          {
-            len++;
-            break ;
-          }
-        }
-        len++;
-        s++;        
-    }
-    return (len);
-}
-
 
 int	ft_printf(const char *str, ...)
 {
 	va_list argp;
 	va_start(argp, str);
-	int	ret = ft_printf_strlen(str);
+	int len;
 
+	len = ft_strlen((str));
 	while (*str != '\0')
 	{
 		if(*str == '%')
 		{
 			str++;
-			ft_check_format(*str, argp);
+			len += ft_check_format(*str, argp);
 		}
 		else 
-		{
 			write(1, &*str, 1);
-		}
 		str++;
 	}
-	return (ret);
+	return (len);
 }
 /*
 int main ()
 {
 	int a = 'f';
 	int b = 42;
-	unsigned int  c = -1;
+	//unsigned int  c = -1;
 
 	ft_printf("Meu printf:\n");
-	ft_printf("%ces\\te\n %s\n %d - %x - %X - %p\n %i - %x - %X - %p\n %i - %d - %u", 't', "teste", b, b, b, &b, a, a, a, &a, c, c, c);
+	int d = ft_printf("Hexa: %x\n", 123456);
+	ft_printf("hexa size: %d\n", d);
+	int e = ft_printf("String: %s\n", "teste");
+	ft_printf("String size: %i\n", e);
+	int f = ft_printf("Char: %c\n", 'c');
+	ft_printf("Char size: %d\n", f);
+	int g = ft_printf("Unsinged int: %u\n", -1);
+	ft_printf("UnInt Size: %d\n", g);
+	int h = ft_printf("Int: %d\n", b);
+	ft_printf("int Size: %d\n", h);
+	int i = ft_printf("Pointer: %p\n", &a);
+	ft_printf("Pointer Size: %d\n", i);
+
+//	int z = ft_printf("\n%ces\\te\n %s\n %d - %x - %X - %p\n %i - %x - %X - %p\n %i - %d - %u", 't', "teste", b, b, b, &b, a, a, a, &a, c, c, c);
+//	ft_printf("\n%i", z);
 	ft_printf("\n------------------------------\n");
-	ft_printf("Printf original:");
-	printf("\n%ces\\te\n %s\n %d - %x - %X - %p\n %i - %x - %X - %p\n %i - %d - %u", 't', "teste", b, b, b, &b, a, a, a, &a, c, c, c);
+	ft_printf("Printf original:\n");
+	int j = printf("Hexa: %x\n", 123456);
+	ft_printf("hexa size: %d\n", j);
+	int k = printf("String: %s\n", "teste");
+	ft_printf("String size: %i\n", k);
+	int l = printf("Char: %c\n", 'c');
+	ft_printf("Char size: %d\n", l);
+	int m = printf("Unsinged int: %u\n", -1);
+	ft_printf("UnInt size: %d\n", m);
+	int n = printf("Int: %d\n", b);
+	ft_printf("Int size: %d\n", n);
+	int o = printf("Pointer: %p\n", &a);
+	ft_printf("Pointer size: %d\n", o);
 	return 0;
 }*/
